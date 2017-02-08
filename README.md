c9b57c6477bd55871bcc1103aaccf3399a1673ba
kubedav <davesade.42@gmail.com>
2017-02-08 16:45:38 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+Endleg - score update
+=====================
+
+This Lambda function serves as main output of scores from the database.  
+It identifies user and then shows his own score, history of playing and similar.  
+
+See [main repository](https://github.com/do-team/endleg) for more details.
---
+++ index.js
@@ -0,0 +1,32 @@
+var AWS = require('aws-sdk');
+
+exports.handler = (event, context, callback) => {
+
+AWS.config.update({
+  region: "eu-central-1",
+  endpoint: "dynamodb.eu-central-1.amazonaws.com"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var table = "endleg-main";
+var user = "test";
+
+var params = {
+    TableName:table,
+    Item:{
+        "user": user,
+        "name": name
+    }
+};
+
+console.log("Getting a new item...");
+docClient.get(params, function(err, data) {
+    if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 1));
+    } else {
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 1));
+    }
+});
+
+};
