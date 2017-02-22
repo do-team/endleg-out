2d837da371cf021bf1ec27b49e41d2cf7539629a
kubedav <davesade.42@gmail.com>
2017-02-22 12:28:11 +0100

    Added sectoken decryption.

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-var AWS = require('aws-sdk');
-
+var AWS = require("aws-sdk");
+var nJwt = require('njwt');
 exports.handler = (event, context, callback) => {
+    AWS.config.update({
+        region: "eu-central-1",
+        endpoint: "dynamodb.eu-central-1.amazonaws.com"
+    });
+    var token = event.headers.sectoken;
+    var signingKey = 'secret';
+    console.log('token: ', token);
 
-AWS.config.update({
-  region: "eu-central-1",
-  endpoint: "dynamodb.eu-central-1.amazonaws.com"
-});
+    nJwt.verify(token, signingKey, function(err, verifiedJwt) {
+            console.log(event);
 
-var docClient = new AWS.DynamoDB.DocumentClient();
+            if (err) {
 
-var table = "endleg-score";
-var user = "test";
+                console.log('Error - but actually it will go trough...', err); // Token has expired, has been tampered with, etc
+                var docClient = new AWS.DynamoDB.DocumentClient();
+                var user = err.parsedBody['cognito:username'];
+                var table = "endleg-score";
+                var params = {
+                     TableName:table,
+                     Key:{
+                         "user": user
+                     }
+                 };
 
-var params = {
-    TableName:table,
-    Key:{
-        "user": user
-    }
-};
-
-console.log("Getting a new item...");
-docClient.get(params, function(err, data) {
-    if (err) {
-        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 1));
-    } else {
-        console.log("GetItem succeeded:", JSON.stringify(data, null, 1));
-    }
-});
-
+                console.log("Reading score from the table...");
+                docClient.get(params, function(err, data) {
+                    if (err) {
+                        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 1));
+                    } else {
+                        console.log("GetItem succeeded:", JSON.stringify(data, null, 1));
+                    }
+                });
+            } else {
+                console.log('All OK decrypted. That also means, it will do nothing with Dynamo :).');
+                console.log(verifiedJwt); // Will contain the header and body
+        }
+    });
 };
