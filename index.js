acf88e4e2406c4bd50fd10d04badefe288303c28
kubedav <davesade.42@gmail.com>
2017-03-02 10:10:54 +0100

    Callback with response.

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ exports.handler = (event, context, callback) => {
                         console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 1));
                     } else {
                         console.log("GetItem succeeded:", JSON.stringify(data, null, 1));
+                        var response = JSON.stringify(data, null, 1);
+                        callback(response);
                     }
                 });
             } else {
